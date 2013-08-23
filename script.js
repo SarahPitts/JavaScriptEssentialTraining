8df04bf935a71b4168651d989481900b3fdc4d24
Sarah Pitts <mccallsmedia@gmail.com>
2013-08-23 16:15:11 -0500

    adds h1 and p text adding nodes manually

--- script.js
+++ script.js
@@ -6,8 +6,15 @@ var newHeading = document.createElement("h1");
 var newParagraph = document.createElement("p");
 
 //add content
-newHeading.innerHTML = "Did you know?";
-newParagraph.innerHTML = "California produces over 17 million gallons of wine each year!";
+//newHeading.innerHTML = "Did you know?";
+//newParagraph.innerHTML = "California produces over 17 million gallons of wine each year!";
+
+//create child nodes manually
+var h1Text = document.createTextNode("Did you know?");
+var paraText = document.createTextNode("California produces over 17 million gallons of wine each year!");
+//add child nodes to the new elements
+newHeading.appendChild(h1Text);
+newParagraph.appendChild(paraText);
 
 //attach the new elements to the document
 document.getElementById("trivia").appendChild(newHeading);
