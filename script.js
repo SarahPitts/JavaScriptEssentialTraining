1ac74c717069b2b205d9357d908ef0e5912c0b42
Sarah Pitts <mccallsmedia@gmail.com>
2013-08-27 12:43:05 -0500

    adds event listeners

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-var mainContent = document.getElementById("mainContent");
-mainContent.setAttribute("align","right");
 
-//create the elements
-var newHeading = document.createElement("h1");
-var newParagraph = document.createElement("p");
 
-//add content
-//newHeading.innerHTML = "Did you know?";
-//newParagraph.innerHTML = "California produces over 17 million gallons of wine each year!";
+// use the commented-out code for reference, or just write your own. 
 
-//create child nodes manually
-var h1Text = document.createTextNode("Did you know?");
-var paraText = document.createTextNode("California produces over 17 million gallons of wine each year!");
-//add child nodes to the new elements
-newHeading.appendChild(h1Text);
-newParagraph.appendChild(paraText);
+//document.onclick = function() {
+//	alert("You clicked somewhere in the document");
+//};
 
-//attach the new elements to the document
-document.getElementById("trivia").appendChild(newHeading);
-document.getElementById("trivia").appendChild(newParagraph);
+//function prepareEventHandlers() {	
+//	var myImage = document.getElementById("mainImage");
+//	myImage.onclick =  function() {
+//		alert("You clicked the image");
+//	}
+//}
 
+//window.onload = function() {
+	// prep anything we need to
+//	prepareEventHandlers();
+//};
