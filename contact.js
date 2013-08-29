28fed117fc1f58c6d0ad522d0699bf5fbe582e80
Sarah Pitts <mccallsmedia@gmail.com>
2013-08-29 15:25:11 -0500

    adds show/hide section to contact page

--- contact.js
+++ contact.js
@@ -18,3 +18,24 @@ function prepareEventHandlers() {
 window.onload =  function() {
 	prepareEventHandlers();
 };
+
+
+
+// show and hide sections of a form
+function preparePage() {
+	document.getElementById("brochures").onclick = function() {
+		if (document.getElementById("brochures").checked) {
+			// use CSS style to show it
+			document.getElementById("tourSelection").style.display = "block";
+		} else {
+			// hide the div
+			document.getElementById("tourSelection").style.display = "none";
+		}
+	};
+	// now hide it on the initial page load.
+	document.getElementById("tourSelection").style.display = "none";
+}
+
+window.onload =  function() {
+	preparePage();
+};
--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-//Events Examples
-//document.onclick = function() {
-//	alert("You clicked somewhere in the document");
-//}
-
-function prepareEventHandlers() {
-
-	var myImage = document.getElementById("mainImage");
-	myImage.onclick = function() {
-	alert("You clicked the image");
-	}
-
+// show and hide sections of a form
+function preparePage() {
+	document.getElementById("brochures").onclick = function() {
+		if (document.getElementById("brochures").checked) {
+			// use CSS style to show it
+			document.getElementById("tourSelection").style.display = "block";
+		} else {
+			// hide the div
+			document.getElementById("tourSelection").style.display = "none";
+		}
+	};
+	// now hide it on the initial page load.
+	document.getElementById("tourSelection").style.display = "none";
 }
 
-window.onload = function() {
-	prepareEventHandlers();
-}
+window.onload =  function() {
+	preparePage();
+};
+
